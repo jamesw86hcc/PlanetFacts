faca223f66bc0ea0e473560f692162513d2d2277
James Williams <marysa.williams@howardcc.edu>
2023-03-31 20:46:36 -0400

    Added Insertion Sort

--- src/Sorting.java
+++ src/Sorting.java
@@ -1,23 +1,22 @@
 import java.util.Arrays;
 
+/*
+* All sorts were referenced from the Yuzu textbook for computer science.
+* Author: James Williams
+* */
 public class Sorting {
 
-    public static void main (String[] args)
-    {
-        int[] inputArray = {6,2,4,3,1};
-        selectionSort(inputArray);
+    public static void main(String[] args) {
+        int[] inputArray = {6, 2, 4, 3, 1};
+        insertionSort(inputArray);
         System.out.println(Arrays.toString(inputArray));
     }
 
-    public static void selectionSort(int[] arr)
-    {
-        for (int i = 0; i < arr.length-1; i++)
-        {
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
             int min_index = i;
-            for (int j = i+1; j<arr.length; j++)
-            {
-                if (arr[min_index] > arr[j])
-                {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[min_index] > arr[j]) {
                     min_index = j;
                 }
 
@@ -26,11 +25,22 @@ public class Sorting {
         }
     }
 
-    public static void insertionSort(int[] arr){
-        //for (int )
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int toInsert = arr[i];
+            int insertIndex = i;
+
+            while (insertIndex > 0 && arr[insertIndex - 1] > toInsert) {
+                // shift element right one slot
+                arr[insertIndex] = arr[insertIndex - 1];
+                insertIndex--;
+            }
+
+            arr[insertIndex] = toInsert; // place inserted element
+        }
     }
 
-    public static void swap(int[] arr, int index1, int index2){
+    public static void swap(int[] arr, int index1, int index2) {
         int temp = arr[index1];
         arr[index1] = arr[index2];
         arr[index2] = temp;
